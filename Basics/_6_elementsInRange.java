177b9686341a1f3272c041d94609bc71f3ce1c2d
VikasKare <karevikas7777@gmail.com>
2023-09-21 20:29:49 +0530

    -

--- Basics/_6_elementsInRange.java
+++ Basics/_6_elementsInRange.java
@@ -4,10 +4,24 @@ class _6_elementsInRange {
 
     static boolean elementRange(int[] arr, int A, int B) {
 
-        int found[] = new int[B - A+ 1];
+      boolean[] inRange = new boolean[B - A + 1];
 
-        for(int i =0; i < )
+      for(int i = 0; i < inRange.length; i++) {
 
+        int num = arr[i];
+
+        if(num >= A && num <= B) {
+            inRange[num - A] = true;
+        }    
+      }
+
+      for(boolean present : inRange) {
+
+            if(!present) {
+
+                return false;
+            }
+      }
         return true;
     }
 
@@ -18,6 +32,14 @@ class _6_elementsInRange {
         System.out.print("Enter size of arary : ");
         int size = sc.nextInt();
 
+        
+        System.out.print("Enter Start of Range : ");
+        int A = sc.nextInt();
+
+        System.out.print("Enter end of Range : ");
+        int B = sc.nextInt();
+        
+
         int[] arr = new int[size];
 
         for(int i = 0; i < arr.length; i++) {
@@ -26,14 +48,13 @@ class _6_elementsInRange {
             arr[i] = sc.nextInt();
         }
 
-        System.out.print("Enter Start of Range : ");
-        int A = sc.nextInt();
 
-        System.out.print("Enter end of Range : ");
-        int B = sc.nextInt();
+        System.out.println(elementRange(arr, A, B));
 
-        int num = A;
+        if(elementRange(arr, A, B) == true) {
 
-        elementRange(arr, A, B);
+            System.out.println("elements are in Range");
+        }
+        
     }   
 }
